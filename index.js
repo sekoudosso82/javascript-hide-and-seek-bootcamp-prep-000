409a69590a015e33dc5cf1888b2f26db2f6cf6d8
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 17:40:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector)
+{
+  const result = document.querySelector(selector)
+  return result
+}
 
+function nestedTarget()
+{
+  var elements = document.getElementsByClassName('target')
+  return elements
+}
+
+function deepestChild()
+{
+  const result= document.querySelector('div."grand-node"  div')
+  return result
+}
